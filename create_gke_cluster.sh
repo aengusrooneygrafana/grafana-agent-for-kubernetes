badc75c74c99bec28664a8e57ce4a9f65ccee4b9
Aengus Rooney <aengus@grafana.com>
2021-05-24 21:54:17 +0200

    nightly build

--- create_gke_cluster.sh
+++ create_gke_cluster.sh
@@ -1,7 +1,7 @@
 export PROJECT_ID=`gcloud config get-value project` && \
   export M_TYPE=n1-standard-2 && \
   export ZONE=us-west2-a && \
-  export THEDATE=`date +%y%m%d%H%M%S`
+  export THEDATE=`date +%y%m%d%H`
   export CLUSTER_NAME=se-rooney-$THEDATE && \
   gcloud services enable container.googleapis.com && \
   gcloud container clusters create $CLUSTER_NAME \
